cd5b3f02227b38aa7627c910622e0f5edfbcba03
Your Name <6206507@mail.ru>
2022-07-14 21:13:52 +0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,121 @@
+if (!Cookies.get("user")) {
+  window.location.replace("/auth.html");
+}
+
+const todos = [
+  "Сделать проектную работу",
+  "Полить цветы",
+  "Пройти туториал по Реакту",
+  "Сделать фронт для своего проекта",
+  "Погулять с собакой",
+  "Разобраться в замыканиях",
+  "Решить задачу на Codewars",
+];
+
+function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+if (!localStorage.getItem("todos")) {
+  setLocalStorage("todos", todos);
+}
+
+// console.log(JSON.parse(data));
+const todoContainers = document.querySelector(".todos__list");
+const todoTemplate = document
+  .querySelector("#todo-item-template")
+  ?.content.querySelector(".todo-item"); //null
+const todoForm = document.querySelector(".todos__form");
+const todoButtonForm = document.querySelector(".todo-form__submit-btn");
+
+function addTaskContainer(taskName, containter, position = "append") {
+  if (position === "append") {
+    return containter.append(createTask(taskName));
+  }
+  if (position === "prepend") {
+    return containter.prepend(createTask(taskName));
+  }
+
+  if (position === "before") {
+    return containter.before(createTask(taskName));
+  }
+  if (position === "after") {
+    return containter.after(createTask(taskName));
+  }
+}
+
+function createTask(taskName) {
+  const todoTaskElement = todoTemplate.cloneNode(true);
+  const todoTaskText = todoTaskElement.querySelector(".todo-item__text");
+  const deletedTaskButton = todoTaskElement.querySelector(".todo-item__del");
+  const clonedTaskButton = todoTaskElement.querySelector(".todo-item__copy");
+  const editedTaskButton = todoTaskElement.querySelector(".todo-item__edit");
+
+  todoTaskText.textContent = taskName;
+
+  deletedTaskButton.addEventListener("click", (e) => {
+    const access = confirm("Вы действительно хотите удалить");
+
+    if (access) {
+      e.target.closest(".todo-item").remove();
+      const oldData = getLocalStorage("todos");
+      console.log(oldData);
+      const newData = oldData.filter((item) => {
+        return item !== taskName;
+      });
+      setLocalStorage("todos", newData);
+      // console.log(newData);
+    }
+  });
+
+  clonedTaskButton.addEventListener("click", (e) => {
+    addTaskContainer(taskName, todoTaskElement, "after");
+  });
+
+  editedTaskButton.addEventListener("click", (event) => {
+    const input = todoForm.querySelector(".todo-form__input");
+    input.value = taskName;
+
+    const button = todoForm.querySelector(".todo-form__submit-btn");
+    button.textContent = "Изменить";
+
+    function editTodo(e) {
+      e.preventDefault();
+      event.target
+        .closest(".todo-item")
+        .querySelector(".todo-item__text").textContent = input.value;
+
+      e.target.reset();
+      button.textContent = "Добавить";
+      todoForm.removeEventListener("submit", editTodo);
+      todoForm.addEventListener("submit", addTodo);
+    }
+
+    todoForm.removeEventListener("submit", addTodo);
+    todoForm.addEventListener("submit", editTodo);
+  });
+
+  return todoTaskElement;
+}
+
+function addTodo(event) {
+  event.preventDefault();
+  const input = event.target.querySelector(".todo-form__input");
+  addTaskContainer(input.value, todoContainers, "prepend");
+}
+
+todoForm.addEventListener("submit", addTodo);
+let localData;
+if (getLocalStorage("todos").length) {
+  localData = getLocalStorage("todos");
+} else {
+  localData = todos;
+}
+
+localData.forEach((text) => {
+  addTaskContainer(text, todoContainers, "append");
+});
